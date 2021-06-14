907f1729dae3e6eaa1cdaeb971a9b25b73095eab
Clara Heinz <35228020+clarakheinz@users.noreply.github.com>
2021-06-14 19:33:58 -0400

    Update for readability

--- JSPetDirectory/js/pets.js
+++ JSPetDirectory/js/pets.js
@@ -17,7 +17,10 @@ let html = '';
 
 for (let i=0; i < pets.length; i++) {
  
-  html += `<h2>${pets[i].name}</h2><h3>${pets[i].type} | ${pets[i].breed}</h3<p>Age: ${pets[i].age}</p><img src="${pets[i].photo}" alt="${pets[i].breed}">`;
+  html += `<h2>${pets[i].name}</h2>
+  <h3>${pets[i].type} | ${pets[i].breed}</h3>
+  <p>Age: ${pets[i].age}</p>
+  <img src="${pets[i].photo}" alt="${pets[i].breed}">`;
 
 }
 
